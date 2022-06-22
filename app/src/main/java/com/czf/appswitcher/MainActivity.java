a1a90abe446c7e783353b78bbd7dde35c2c76834
chenzhifei <chenzhifei.007@bytedance.com>
2022-06-22 11:20:40 +0800

    init

---
+++ app/src/main/java/com/czf/appswitcher/MainActivity.java
@@ -0,0 +1,14 @@
+package com.czf.appswitcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
